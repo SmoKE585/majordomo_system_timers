c7ee479ae5e21505f1ac214227a306ac3c6cfe10
SmoKE585 <53314888+SmoKE585@users.noreply.github.com>
2020-03-04 21:23:07 +0300

    v.1.9
    - Add days

--- modules/system_timers/system_timers.class.php
+++ modules/system_timers/system_timers.class.php
@@ -4,7 +4,7 @@ class system_timers extends module {
     $this->name="system_timers";
     $this->title="Системные таймеры";
     $this->module_category="<#LANG_SECTION_SYSTEM#>";
-    $this->version="1.8";
+    $this->version="1.9";
     $this->checkInstalled();
   }
 
